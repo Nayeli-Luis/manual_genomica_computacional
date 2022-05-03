f747a8020a357660d83db564ecff0332997b73ab
Nayeli Luis <37789254+Nayeli-Luis@users.noreply.github.com>
2022-05-02 19:43:32 -0500

    practica 2: bash y ngs

---
+++ _build/jupyter_execute/04_practicas/p2_bash_ngs.sh
@@ -0,0 +1,41 @@
+echo "Hola mundo"
+
+# Comando para crear directorio
+
+# Comando para ingresar a directorio
+
+# Comando para imprimir ruta
+
+# Comando para conocer shell
+
+# Comando para crear directorios
+
+# Comando para mover raw_data y filtered
+
+# Comando para mover archivos
+
+# Comando para listado largo y legible para humanos
+
+# Comando para copiar archivo gff
+
+# Comando para ver fastq
+
+# Comando para ver fasta
+
+# Comando para ver gff
+
+# Comando para contar secuencias en fastq
+
+# Comando para contar secuencias en fasta
+
+# less archivo.gff3
+
+# less -S archivo.gff3
+
+# Comando
+
+# Comando para cambiar nombre de un archivo
+
+# Comando para concer cuántos genes hay
+
+# Comando
